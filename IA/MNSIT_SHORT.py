7831a83f61459ed5178c43063f545972b343d134
PearlClone <benjaminlecomte37@gmail.com>
2023-04-02 15:27:42 +0200

    update

--- IA/MNSIT_SHORT.py
+++ IA/MNSIT_SHORT.py
@@ -17,7 +17,7 @@ model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=60)
+model.fit(x_train, y_train, epochs=150)
 
 model.summary()
 
