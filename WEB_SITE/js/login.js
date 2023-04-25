da73c85b65fca3dc6c9e00c58f47faa5691760ca
PearlClone <benjaminlecomte37@gmail.com>
2023-04-25 18:51:43 +0200

    update

--- WEB_SITE/js/login.js
+++ WEB_SITE/js/login.js
@@ -1,6 +1,8 @@
 const logregBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const signInBtn = document.querySelector('#btn-in');
+const signUpBtn = document.querySelector('#btn-up');
 
 registerLink.addEventListener('click', () => {
     logregBox.classList.add('active');
@@ -9,3 +11,42 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
+
+// Ajouter un événement onclick
+signInBtn.onclick = function(event) {
+  // Récupérer la valeur de l'input email
+  const emailInput = document.querySelector('#email-in');
+  const email = emailInput.value.trim();
+
+  // Vérifier si l'email est valide
+  if (!isValidEmail(email)) {
+    // Empêcher l'action par défaut du bouton
+    event.preventDefault();
+
+    // Afficher un message d'erreur
+    alert('Please enter a valid email address');
+  }
+}
+
+// Ajouter un événement onclick
+signUpBtn.onclick = function(event) {
+    // Récupérer la valeur de l'input email
+    const emailUpput = document.querySelector('#email-up');
+    const email = emailUpput.value.trim();
+  
+    // Vérifier si l'email est valide
+    if (!isValidEmail(email)) {
+      // Empêcher l'action par défaut du bouton
+      event.preventDefault();
+  
+      // Afficher un message d'erreur
+      alert('Please enter a valid email address');
+    }
+  }
+
+// Fonction pour valider l'email
+function isValidEmail(email) {
+    // Utiliser une expression régulière pour valider l'email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
